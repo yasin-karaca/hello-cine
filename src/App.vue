8d9cb450b1bb70cbf45d94230ec86f1ad601b943
yasin-karaca <hello.yasinkaraca@gmail.com>
2026-07-08 18:28:09 +0200

    add types for movie response

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useFetch } from '@/composables/useApi'
+import { onMounted } from 'vue'
+
+const { data, execute } = useFetch('https://api.themoviedb.org/3/genre/movie/list')
+
+onMounted(async () => {
+  await execute()
+})
+</script>
 
 <template>
   <RouterView />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,10 @@
   <div>
     <h1>Top rated movies</h1>
     <template v-if="data">
-      <div v-for="movie in data.results">
+      <div v-for="movie in data.results" :key="movie.id">
         <RouterLink :to="`/movie/${movie.id}`">
+          <h2>{{ movie.title }}</h2>
+
           <pre>{{ movie }}</pre>
           <img :src="getImageUrl(movie.poster_path)" :alt="movie.title" />
         </RouterLink>
@@ -27,7 +29,7 @@ const currentPage = ref(1)
 const getUrl = (page: number) =>
   `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}`
 
-const { data, execute } = useFetch(getUrl(currentPage.value))
+const { data, execute } = useFetch<MovieResponse>(getUrl(currentPage.value))
 
 watch(currentPage, async (newPage) => {
   await execute(getUrl(newPage))
@@ -35,4 +37,20 @@ watch(currentPage, async (newPage) => {
 
 const nextPage = () => currentPage.value++
 const prevPage = () => currentPage.value--
+
+export interface Movie {
+  id: number
+  title: string
+  poster_path: string
+  overview: string
+  vote_average: number
+  release_date: string
+}
+
+export interface MovieResponse {
+  results: Movie[]
+  total_pages: number
+  total_results: number
+  page: number
+}
 </script>
